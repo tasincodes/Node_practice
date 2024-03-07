71e6349dae0bb7d3410c6986c2e8c88c11c2f9f7
tasincodes <md.shahrier.tasin@g.bracu.ac.bd>
2024-03-07 16:15:10 +0600

    validation done

--- My-api/src/models/StudentsModel.js
+++ My-api/src/models/StudentsModel.js
@@ -3,8 +3,24 @@ const mongoose=require('mongoose')
 const dataSchema = mongoose.Schema({
     name :{type:String},
     id :{type:String},
-    sec:{type:Number,min:6,max:8},
-    remarks : {type:String,default:"No default value"}
+    sec:{type:Number,
+   min:[6,"minimum value 6 but got value = {VALUE}"],
+    max:[12,"maximum value 12 but got value ={VALUE}"]
+    },
+    remarks : {type:String,default:"No default value"},
+    Mobile :{
+        type:String,
+        // validate: {
+        //     validator: (value) =>{
+        //         return /(^(\+88|0088)?(01){1}[3456789]{1}(\d){8})$/.test(value)
+        //     },
+        //     message:"11 digit mobile num required"
+        //   }
+        validate: {
+            validator: (value) => value !== undefined && value.length === 11,
+            message: 'Mobile number must be 11 digits long'
+          }
+    }
 
 },{versionKey:false})
 
